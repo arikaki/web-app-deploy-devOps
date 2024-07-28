522d32209471bace3ae0746ce27914530c6ac531
SaiRishab <78468475+arikaki@users.noreply.github.com>
2024-07-28 13:28:45 -0400

    made changes in main.go file and URL in README.md file

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ To run the server, execute the following command:
 go run main.go
 ```
 
-The server will start on port 8080. You can access it by navigating to `http://localhost:8080` in web browser.
+The server will start on port 8080. You can access it by navigating to `http://localhost:8080/static` in web browser.
 
 # DevOps Implementation for Go Web Application
 
--- main.go
+++ main.go
@@ -5,14 +5,22 @@ import (
 	"net/http"
 )
 
+func homePage(w http.ResponseWriter, r *http.Request) {
+	// Render the home HTML page from static folder
+	http.ServeFile(w, r, "static/public/index.html")
+}
+
 func main() {
-	// Serve static files from the "public" directory
-	fs := http.FileServer(http.Dir("/static/public"))
-	http.Handle("/", fs)
+	// Serve static files from the "static/public" directory
+	fs := http.FileServer(http.Dir("static/public"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Serve the home page
+	http.HandleFunc("/", homePage)
 
 	// Start the server on port 8080
 	log.Println("Listening on :8080...")
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
